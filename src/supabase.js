232887e3ca728120ab20bec50acba428e73f92a2
Alfonso Hidalgo <bookvippoints@gmail.com>
2026-06-21 00:24:49 -0500

    Fix supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,4 @@
- 
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = 'https://jsvzljwfuksneglmcwuc.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzdnpsandmdWtzbmVnbG1jd3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5OTc3MzksImV4cCI6MjA5NzU3MzczOX0. 
