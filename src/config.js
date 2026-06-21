4f47a4692cce032fb8e81c9769ad2bcc0cd0bac7
bookvippoints-design <bookvippoints@gmail.com>
2026-06-21 08:18:56 +0000

    Actualizar REGISTRO_URL a panel-pasaporte593.netlify.app

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ export const WA_URL = `https://wa.me/${WA_NUMBER}?text=${WA_MSG}`
 export const WA_ICON = '💬'
 export const PANEL_URL = 'https://panel-pasaporte593.netlify.app'
 export const BVP_URL = 'https://bvpoints.netlify.app'
-export const REGISTRO_URL = 'https://panelpromo593.netlify.app'
+export const REGISTRO_URL = 'https://panel-pasaporte593.netlify.app'
