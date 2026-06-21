fbf6c2f4f0bfe0c459be9aa8d4230d6ea95fe99a
Alfonso Hidalgo <bookvippoints@gmail.com>
2026-06-21 00:31:22 -0500

    Fix comillas supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://jsvzljwfuksneglmcwuc.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzdnpsandmdWtzbmVnbG1jd3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5OTc3MzksImV4cCI6MjA5NzU3MzczOX0. 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzdnpsandmdWtzbmVnbG1jd3VjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5OTc3MzksImV4cCI6MjA5NzU3MzczOX0._uPBW8jRj1qOCYqdOYKnMWe1zC85kQ8wsA8K22GqbLE'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
